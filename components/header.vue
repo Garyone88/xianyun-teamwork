a834afb799d87add6ff6c9593209e1a04d4c2af4
Garyone88 <gary335@126.com>
2019-06-28 19:00:01 +0800

    初步完成首页

---
+++ components/header.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="header">
+    <!-- 中间的内容 -->
+    <el-row type="flex" justify="space-between" class="main">
+      <!-- logo -->
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="/images/logo.jpg" alt>
+        </nuxt-link>
+      </div>
+
+      <!-- 菜单 -->
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+
+      <!-- 登录状态 -->
+      <el-row>
+        <!-- 下拉菜单 -->
+        <el-dropdown v-if="true">
+          <span class="el-dropdown-link">
+            <img src="http://157.122.54.189:9095/assets/images/avatar.jpg" alt>
+
+            <span>地球发动机</span>
+
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+
+        <div v-else class="login">
+          <nuxt-link to="/user/login">登录 / 注册</nuxt-link>
+        </div>
+      </el-row>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.header {
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px #ddd solid;
+  box-shadow: 0 2px 2px #ddd;
+  .main {
+    width: 1000px;
+    height: 60px;
+    margin: 0 auto;
+    .logo {
+      margin-top: 9px;
+      width: 156px;
+      height: 43px;
+      img {
+        width: 100%;
+      }
+    }
+    .navs {
+      flex: 1;
+      height: 60px;
+      line-height: 60px;
+      a {
+        display: block;
+        box-sizing: border-box;
+        padding: 0 20px;
+        height: 100%;
+        &:hover {
+          border-bottom: 5px solid #409eff;
+          color: #409eff;
+        }
+      }
+      .nuxt-link-exact-active {
+        background-color: #409eff;
+        color: #fff;
+        &:hover {
+          border-bottom: none;
+          color: #fff;
+        }
+      }
+    }
+    .el-dropdown-link {
+      line-height: 60px;
+      img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border: 1px solid transparent;
+        vertical-align: middle;
+        &:hover {
+          border: 1px solid #409eff;
+        }
+      }
+    }
+    .login {
+      line-height: 60px;
+      &:hover {
+        color: #409eff;
+      }
+    }
+  }
+}
+</style>
+
